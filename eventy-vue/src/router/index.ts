834dceddfccd2d1fba1db00af1496de654eda2c3
unknown <romake@ttu.ee>
2022-08-09 19:39:04 +0300

    Event Related Pages Initialization (Management, Overview and My Events).

--- eventy-vue/src/router/index.ts
+++ eventy-vue/src/router/index.ts
@@ -6,17 +6,27 @@ import Home from "@/views/Home.vue"
 import Login from "@/views/Account/Login.vue";
 import Signup from "@/views/Account/Signup.vue";
 
+import MyEvents from "@/views/event/MyEvents.vue";
+import EventOverview from "@/views/event/EventOverview.vue";
+import EventManagement from "@/views/event/EventManagement.vue";
+
+import NotFound from '@/views/NotFound.vue';
+
 
 /**
  * Application Routes With Their Configurations.
  */
 const routes = [
 
+  // Basic Routes Registration.
   {
     path: '/',
     name: 'home',
     component: Home
   },
+
+
+  // Account Related Routes Registration.
   {
     path: '/login',
     name: 'login',
@@ -26,6 +36,34 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: Signup
+  },
+
+
+  // Event Related Routes Registration.
+  {
+    path: '/manage-event/:id',
+    name: 'event-management',
+    component: EventManagement,
+    props: true
+  },
+  {
+    path: '/event/:id',
+    name: 'event-overview',
+    component: EventOverview,
+    props: true
+  },
+  {
+    path: '/myevents',
+    name: 'myevents',
+    component: MyEvents,
+  },
+
+
+  // 404 Not Found Route Registration.
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: NotFound
   },
 ]
 
---
+++ eventy-vue/src/views/NotFound.vue
@@ -0,0 +1,16 @@
+
+<template>
+    404
+</template>
+
+
+<script lang="ts">
+
+  export default {};
+
+</script>
+
+
+<style scoped>
+
+</style>
