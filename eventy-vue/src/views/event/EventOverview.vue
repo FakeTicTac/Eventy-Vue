d1db8686a5d9b9ec497b6ed773e80ee552ffac5e
unknown <romake@ttu.ee>
2022-08-09 20:00:13 +0300

    Basic Layout For Event Related Pages (Overview, Management and MyEvents)

--- eventy-vue/src/views/event/EventOverview.vue
+++ eventy-vue/src/views/event/EventOverview.vue
@@ -1,6 +1,25 @@
 
 <template>
-
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <img src="https://img.freepik.com/free-vector/music-event-poster-template-with-colorful-shapes_1361-1591.jpg?w=2000" alt="Italian Trulli" class="img-fluid w-100">
+      <h2>12 July 2022</h2>
+      <h1>ELECTRONIC SOUND</h1>
+    </div>
+  </div>
+  <div class="row text-left">
+    <div class="col-md-12">
+        <h6>What will wait You there?</h6>
+        <p>At the exhibition, visitors will become acquainted with the event’s historical background, witness the events of the eve of the evacuation and the heat of battle, and reflect on the aftermath of the tragedy that persists to this day.
+The exhibition focuses mainly on people’s stories and the naval mines that caused a lot of doom. Visitors can explore two ships that escaped from the evacuation – the submarine Lembit and the icebreaker Suur Tõll are part of the museum’s permanent exhibition.
+</p>
+  <div class="row text-center">
+    <div class="col-md-12">
+      <h2>Raja 4D 203A, Tallinn, Estonia</h2>
+    </div>
+  </div>
+    </div>
+  </div>
 </template>
 
 
--- eventy-vue/src/views/event/MyEvents.vue
+++ eventy-vue/src/views/event/MyEvents.vue
@@ -1,6 +1,79 @@
 
 <template>
+  <div class="row text-center mt-5 mb-5">
+    <div class="column-md-12">
+      <div class="input-group">
+          <input type="text" class="form-control" />
+          <div class="input-group-btn">
+            <button type="submit" class="btn">Search</button>
+          </div>
+        </div>
+    </div>
+  </div>
 
+  <div class="row text-center">
+    <div class="col-md-3">
+
+    </div>
+    <div class="col-md-3">
+      <h6>Name</h6>
+    </div>
+    <div class="col-md-3">
+      <h6>Date</h6>
+    </div>
+    <div class="col-md-3">
+      <h6>Place</h6>
+    </div>
+  </div>
+  <hr/>
+
+  <div class="row text-center">
+    <div class="col-md-3 my-auto">
+      <img src="https://img.freepik.com/free-vector/music-event-poster-template-with-colorful-shapes_1361-1591.jpg?w=2000" alt="Italian Trulli" class="img-fluid w-100"/>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Electronic Sound</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>12 July 2022</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Raja 4D, Tallinn, Estonia</h6>
+    </div>
+  </div>
+  <hr/>
+
+    <div class="row text-center">
+    <div class="col-md-3 my-auto">
+      <img src="https://img.freepik.com/free-vector/music-event-poster-template-with-colorful-shapes_1361-1591.jpg?w=2000" alt="Italian Trulli" class="img-fluid w-100"/>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Electronic Sound</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>12 July 2022</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Raja 4D, Tallinn, Estonia</h6>
+    </div>
+  </div>
+  <hr/>
+
+    <div class="row text-center">
+    <div class="col-md-3 my-auto">
+      <img src="https://img.freepik.com/free-vector/music-event-poster-template-with-colorful-shapes_1361-1591.jpg?w=2000" alt="Italian Trulli" class="img-fluid w-100"/>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Electronic Sound</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>12 July 2022</h6>
+    </div>
+    <div class="col-md-3 my-auto">
+      <h6>Raja 4D, Tallinn, Estonia</h6>
+    </div>
+  </div>
+  <hr/>
 </template>
 
 
